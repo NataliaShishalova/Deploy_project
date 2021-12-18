656900e791b187460d5e90b5bdd82ab892823e9d
Natalia Shishalova <natalia.shishalova@gmail.com>
2021-12-18 21:43:41 +0300

    Add model_list and predict_model_name

--- main.py
+++ main.py
@@ -4,6 +4,9 @@ import pickle
 import pandas as pd
 import json
 from io import BytesIO
+from os import listdir
+from os.path import isfile, join
+from pathlib import Path
 
 app = Flask(__name__)
 
@@ -28,10 +31,24 @@ def predict_2():
     return json.dumps(prediction)
 
 
+@app.route('/predict_model_name', methods=['POST'])
+def predict_model_name():
+    model_name = request.args.get('name')
 
-@app.route('/')
-def index():
-    return "Hello, World!"
+    data_io = BytesIO(request.data)
+    data_test = pd.read_csv(data_io, index_col='Id')
+    test_features = data_test.drop(columns=['Open Date', 'City', 'City Group', 'Type'])
+
+    model = pickle.load(open("models/" + model_name + ".pkl", "rb"))
+
+    prediction = model.predict(test_features).tolist()
+
+    return json.dumps(prediction)
+
+@app.route('/model_list', methods=['GET'])
+def model_list():
+    m_list = [Path(f).stem for f in listdir("models") if isfile(join("models", f))]
+    return json.dumps(m_list)
 
 if __name__ == '__main__':
     app.run("0.0.0.0")
