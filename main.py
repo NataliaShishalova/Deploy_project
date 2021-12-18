06581b0fbace89d7e5bf9ee05544753a2e2005e7
Natalia Shishalova <natalia.shishalova@gmail.com>
2021-12-18 19:41:28 +0300

    predict req

--- main.py
+++ main.py
@@ -1,27 +1,40 @@
 from flask import Flask
 from flask import request
+import pickle
+import pandas as pd
 import json
+from io import BytesIO
 
 app = Flask(__name__)
 
 @app.route('/predict', methods=['GET'])
 def predict():
     model = pickle.load(open("models/lg_model.pkl", "rb"))
-    prediction = model.predict("data/test.scv")
-    return prediction
+    data_test = pd.read_csv('Data/test.csv', index_col='Id')
+    test_features = data_test.drop(columns=['Open Date', 'City', 'City Group', 'Type'])
+    prediction = model.predict(test_features).tolist()
+    return json.dumps(prediction)
 
-#@app.route('/model_name/predict', methods=['GET'])
-#def model_name_predict():
-#    model = pickle.load(open("models/lg_model.pkl", "rb"))
-#    prediction = model.predict("data/test.scv")
-#    return prediction
+@app.route('/predict_2', methods=['POST'])
+def predict_2():
+    data_io = BytesIO(request.data)
+    data_test = pd.read_csv(data_io, index_col='Id')
+
+    test_features = data_test.drop(columns=['Open Date', 'City', 'City Group', 'Type'])
+    model = pickle.load(open("models/lg_model.pkl", "rb"))
+
+    prediction = model.predict(test_features).tolist()
+
+    return json.dumps(prediction)
+
+
 
 @app.route('/')
-def index()
+def index():
     return "Hello, World!"
 
 if __name__ == '__main__':
-    app.run()
+    app.run("0.0.0.0")
 
 
 
